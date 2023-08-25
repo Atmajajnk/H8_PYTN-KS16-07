aae50e829dd68a09182c081f5c50e0cc9e2db14c
Atmajajnk <atmajajnk69@gmail.com>
2023-08-25 21:21:18 +0700

    add sesi 3 and Readme

---
+++ mod.py
@@ -0,0 +1,17 @@
+import mod
+import sys
+sys.path
+import re
+re.__file__
+s = "Hacktiv8-PTP Python For Data Science" 
+
+a = [100, 200, 300]
+def foo(arg):
+    print (f'arg {arg}')
+
+class Foo: 
+    pass
+
+print(mod.s)
+print(mod.a)
+print(mod.foo)
